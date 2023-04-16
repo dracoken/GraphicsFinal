615e912da2244c4beab8cba5a67fbd3490fd8951
dracoken <113327380+dracoken@users.noreply.github.com>
2023-04-16 03:15:43 -0400

    quickly made server
    we still got to make what ever html file we are going to use, but i can prob manage to do that for you

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("inside");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res) => {
+    res.status(200);
+    res.send("test landing page");
+});
+
+app.listen(port, () => {
+    console.log("listiening on port " + port);
+}); 
