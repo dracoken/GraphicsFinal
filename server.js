01d9cd2446fe260474bf4ce4f544f1fb87e221af
ToadallyPog <114781827+ToadallyPog@users.noreply.github.com>
2023-04-27 22:58:10 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3003
 
-app.get('/',(req,res) => {
-    res.status(200);
-    res.send("test landing page");
-});
+const HELP_STRING = "";
+
+
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
+app.get('/help', (req, res) => res.send(HELP_STRING));
+app.use('/', express.static('..'));
 
 app.listen(port, () => {
-    console.log("listiening on port " + port);
-}); 
+  console.log(`WebGLServer listening on port ${port}`)
+})
+
+
+/* const express = require('express')
+const app = express()
+const port = 3003
+
+const HELP_STRING = "";
+
+
+app.get('/favicon.ico', (req, res) => res.sendStatus(204));
+app.get('/help', (req, res) => res.send(HELP_STRING));
+app.use('/', express.static('..'));
+
+
+app.get('/', (req, res) => 
+  {
+    res.sendFile('./lab10.html', { root: '.' })
+  });
+
+app.listen(port, () => {
+  console.log(`WebGLServer listening on port ${port}`)
+})
+*/
